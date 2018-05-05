feeff846bf60573bf165c052f9f6d4bf07881cba
Katy Rumble <rumblec@sunyulster.edu>
2018-05-05 12:47:29 -0400

    Adding Project

---
+++ Apple Picker Prototype/Assets/Apple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public static float bottomY = -20f;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+		
+
+			// Get a reference to the ApplePicker componenet
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+			apScript.AppleDestroyed (); // call AppleDestoryed method in
+								    	// the ApplePicker script
+		}
+		
+	}
+}
